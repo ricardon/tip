3e8b1f8ce69e2b4ea18328190450ab22be1a712a
Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
2019-04-19 17:55:35 -0700

    x86/watchdog: Add a shim hardlockup detector
    The generic hardlockup detector is based on perf. It also provides a set
    of weak functions that CPU architectures can override. Add a shim
    hardlockup detector for x86 that overrides such functions and can
    select between perf and HPET implementations of the detector.
    
    For clarity, add the intermediate Kconfig symbol X86_HARDLOCKUP_DETECTOR
    that is selected whenever the core of the hardlockup detector is
    selected.
    
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Stephane Eranian <eranian@google.com>
    Cc: "Ravi V. Shankar" <ravi.v.shankar@intel.com>
    Cc: iommu@lists.linux-foundation.org
    Cc: linuxppc-dev@lists.ozlabs.org
    Cc: x86@kernel.org
    Suggested-by: Nicholas Piggin <npiggin@gmail.com>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
    ---
    Changes since v5:
     * Added watchdog_nmi_start() to be used when tsc_khz is recalibrated.
     * Always build the x86-specific hardlockup detector shim; not only
       when the HPET-based detector is selected.
     * Corrected a typo in comment in watchdog_nmi_probe() (Ani)
     * Removed useless local ret variable in watchdog_nmi_enable(). (Ani)
    
    Changes since v4:
     * Use a switch to enable and disable the various available detectors.
       (Andi)
    
    Changes since v3:
     * Fixed style in multi-line comment. (Randy Dunlap)
    
    Changes since v2:
     * Pass cpu number as argument to hardlockup_detector_[enable|disable].
       (Thomas Gleixner)
    
    Changes since v1:
     * Introduced this patch: Added an x86-specific shim hardlockup
       detector. (Nicholas Piggin)

---
+++ arch/x86/kernel/watchdog_hld.c
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * A shim hardlockup detector. It overrides the weak stubs of the generic
+ * implementation to select between the perf- or the hpet-based implementation.
+ *
+ * Copyright (C) Intel Corporation 2022
+ */
+
+#include <linux/nmi.h>
+#include <asm/hpet.h>
+
+enum x86_hardlockup_detector {
+	X86_HARDLOCKUP_DETECTOR_PERF,
+	X86_HARDLOCKUP_DETECTOR_HPET,
+};
+
+static enum __read_mostly x86_hardlockup_detector detector_type;
+
+int watchdog_nmi_enable(unsigned int cpu)
+{
+	switch (detector_type) {
+	case X86_HARDLOCKUP_DETECTOR_PERF:
+		hardlockup_detector_perf_enable();
+		break;
+	case X86_HARDLOCKUP_DETECTOR_HPET:
+		hardlockup_detector_hpet_enable(cpu);
+		break;
+	default:
+		return -ENODEV;
+	}
+
+	return 0;
+}
+
+void watchdog_nmi_disable(unsigned int cpu)
+{
+	switch (detector_type) {
+	case X86_HARDLOCKUP_DETECTOR_PERF:
+		hardlockup_detector_perf_disable();
+		break;
+	case X86_HARDLOCKUP_DETECTOR_HPET:
+		hardlockup_detector_hpet_disable(cpu);
+		break;
+	}
+}
+
+int __init watchdog_nmi_probe(void)
+{
+	int ret;
+
+	/*
+	 * Try first with the HPET hardlockup detector. It will only
+	 * succeed if selected at build time and requested in the
+	 * nmi_watchdog command-line parameter. This ensures that the
+	 * perf-based detector is used by default, if selected at
+	 * build time.
+	 */
+	ret = hardlockup_detector_hpet_init();
+	if (!ret) {
+		detector_type = X86_HARDLOCKUP_DETECTOR_HPET;
+		return ret;
+	}
+
+	ret = hardlockup_detector_perf_init();
+	if (!ret) {
+		detector_type = X86_HARDLOCKUP_DETECTOR_PERF;
+		return ret;
+	}
+
+	return 0;
+}
+
+void watchdog_nmi_stop(void)
+{
+	/* Only the HPET lockup detector defines a stop function. */
+	if (detector_type == X86_HARDLOCKUP_DETECTOR_HPET)
+		hardlockup_detector_hpet_stop();
+}
+
+void watchdog_nmi_start(void)
+{
+	/* Only the HPET lockup detector defines a start function. */
+	if (detector_type == X86_HARDLOCKUP_DETECTOR_HPET)
+		hardlockup_detector_hpet_start();
+}
