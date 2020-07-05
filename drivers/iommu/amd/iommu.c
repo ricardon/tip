99f020052106013d3e522dc3604b13207ea1c6c7
Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
2020-07-05 15:40:35 -0700

    iommu/amd: Fixup delivery mode of the HPET hardlockup interrupt
    The HPET hardlockup detector requires that the HPET timer delivers the
    interrupt as NMI. When interrupt remapping is disabled, this can be
    done by programming the HPET MSI registers directly. With interrupt
    remapping, it is necessary to populate an entry in the interrupt
    remapping table.
    
    In x86 there is not an IRQF_NMI flag that can be used to indicate the
    delivery mode when requesting an interrupt (via request_irq()). Thus,
    there is no way for the interrupt remapping driver to know and set
    the delivery mode.
    
    Hence, when allocating an interrupt, check if such interrupt belongs to
    the HPET hardlockup detector and fixup the delivery mode accordingly.
    
    Cc: Ashok Raj <ashok.raj@intel.com>
    Cc: Andi Kleen <andi.kleen@intel.com>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: Borislav Petkov <bp@suse.de>
    Cc: Jacob Pan <jacob.jun.pan@intel.com>
    Cc: Joerg Roedel <joro@8bytes.org>
    Cc: Juergen Gross <jgross@suse.com>
    Cc: Bjorn Helgaas <bhelgaas@google.com>
    Cc: Wincy Van <fanwenyi0529@gmail.com>
    Cc: Kate Stewart <kstewart@linuxfoundation.org>
    Cc: Philippe Ombredanne <pombredanne@nexb.com>
    Cc: "Eric W. Biederman" <ebiederm@xmission.com>
    Cc: Baoquan He <bhe@redhat.com>
    Cc: Jan Kiszka <jan.kiszka@siemens.com>
    Cc: Lu Baolu <baolu.lu@linux.intel.com>
    Cc: Stephane Eranian <eranian@google.com>
    Cc: Suravee Suthikulpanit <Suravee.Suthikulpanit@amd.com>
    Cc: "Ravi V. Shankar" <ravi.v.shankar@intel.com>
    Cc: x86@kernel.org
    Cc: iommu@lists.linux-foundation.org
    Signed-off-by: Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
    ---
    Changes since v5:
     * Use the renamed function is_hpet_irq_hardlockup_detector().
    
    Changes since v4:
     * Introduced this patch.
    
    Changes since v3:
     * N/A
    
    Changes since v2:
     * N/A
    
    Changes since v1:
     * N/A

--- drivers/iommu/amd/iommu.c
+++ drivers/iommu/amd/iommu.c
@@ -35,6 +35,7 @@
 #include <asm/irq_remapping.h>
 #include <asm/io_apic.h>
 #include <asm/apic.h>
+#include <asm/hpet.h>
 #include <asm/hw_irq.h>
 #include <asm/proto.h>
 #include <asm/iommu.h>
@@ -3254,6 +3255,14 @@ static int irq_remapping_alloc(struct irq_domain *domain, unsigned int virq,
 		irq_data->hwirq = (devid << 16) + i;
 		irq_data->chip_data = data;
 		irq_data->chip = &amd_ir_chip;
+
+		/*
+		 * If we find the HPET hardlockup detector irq, fixup the
+		 * delivery mode.
+		 */
+		if (is_hpet_irq_hardlockup_detector(info))
+			cfg->delivery_mode = APIC_DELIVERY_MODE_NMI;
+
 		irq_remapping_prepare_irte(data, cfg, info, devid, index, i);
 		irq_set_status_flags(virq + i, IRQ_MOVE_PCNTXT);
 	}
